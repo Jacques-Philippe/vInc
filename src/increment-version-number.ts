bf86d9f0fb27c3addf15839400d0802020cf091b
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-01-03 14:23:52 -0500

    Increment version number functionality
    Defined functionality for incrementing version number and corresponding tests.

---
+++ src/increment-version-number.ts
@@ -0,0 +1,37 @@
+import { CommandLineInput } from "./io";
+
+const toVersionNumber = (major: string, minor: string, patch: string): string =>
+  `${major}.${minor}.${patch}`;
+
+const increment = (original_value: string): string =>
+  `${parseInt(original_value) + 1}`;
+
+/**
+ *
+ * @param version_number the version number before it is incremented (in trimmed form, without the leading v. For instance 1.2.3 is a valid argument, but v1.2.3 is not)
+ * @returns the incremented version number
+ * @throws error on incorrect input received
+ */
+export const incrementVersionNumber = (
+  input: CommandLineInput,
+  version_number: string
+): string => {
+  const [major, minor, patch] = version_number.split(".");
+
+  switch (input) {
+    case CommandLineInput.MAJOR: {
+      return toVersionNumber(increment(major), minor, patch);
+    }
+    case CommandLineInput.MINOR: {
+      return toVersionNumber(major, increment(minor), patch);
+    }
+    case CommandLineInput.PATCH: {
+      return toVersionNumber(major, minor, increment(patch));
+    }
+    default: {
+      throw new Error(
+        `Expected command line input major, minor, or patch. Instead received ${input}`
+      );
+    }
+  }
+};
