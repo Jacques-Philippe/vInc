4850e4f8123cc9002fd645b85b0aeff876775e16
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-01-15 20:17:58 -0500

    Add filename specification
    Added the possibility for the user to specify a filename as argument to the application.

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,26 @@ import {
 } from "./io";
 import { writeNewVersionToFile } from "./write-new-version-to-file";
 (async () => {
+  const args = process.argv.slice(2);
+  const filename = args[0];
+  if (filename === undefined) {
+    console.log("Please enter a filename as argument");
+    ExitProgram();
+  }
+
   //Gather input to know what to increment
   const input = await GetCommandLineInput();
   // console.log(`Chose ${input}`)
   if (input === CommandLineInput.CANCEL) ExitProgram();
 
   //Read file and return version number
-  const old_version_number = await getVersionNumber(INPUT_FILENAME);
+  const old_version_number = await getVersionNumber(filename);
 
   const new_version_number = incrementVersionNumber(input, old_version_number);
 
   //Increment the number
   console.log(`${old_version_number} -> ${new_version_number}`);
 
-  await writeNewVersionToFile(new_version_number, INPUT_FILENAME);
-  console.log(`Version number written to file ${INPUT_FILENAME}`);
+  await writeNewVersionToFile(new_version_number, filename);
+  console.log(`Version number written to file ${filename}`);
 })();
