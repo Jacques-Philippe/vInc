4c0c6290e0e92a78281e58437f93c4ebea4029d8
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-01-15 20:24:53 -0500

    Add stricter commit policy to precommit hook

--- scripts/execute_tests.sh
+++ scripts/execute_tests.sh
@@ -13,9 +13,23 @@ run_test(){
     #else, varying degrees of failure
     exit $RESULT
 }
-num_modified_ts_files=$(git diff --name-only --cached | grep *.ts -c)
 
-if [ $num_modified_ts_files -gt 0 ]
+num_untracked_ts_files=$(git ls-files --others --exclude-standard | grep *.ts -c)
+if [ $num_untracked_ts_files -gt 0 ]
+then
+    echo "❌ You may not commit with any untracked .ts files in the local repo"
+    exit 2
+fi
+
+num_unstaged_ts_files=$(git diff --name-only | grep *.ts -c)
+if [ $num_unstaged_ts_files -gt 0 ]
+then
+    echo "❌ You may not commit with any unstaged .ts files in the local repo"
+    exit 2
+fi
+
+num_staged_ts_files=$(git diff --name-only --cached | grep *.ts -c)
+if [ $num_staged_ts_files -gt 0 ]
 then
     echo "👀 Staged .ts files detected. Executing tests..."
     run_test
